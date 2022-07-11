4e07f13ba0cfb684231772ef1c4d674f1c06e256
Samuel-Igwudu <igwudu03@gmail.com>
2022-07-11 04:46:15 -0700

    a function that produces output according to a format

--- main.h
+++ main.h
@@ -1,5 +1,31 @@
-#ifundef _MAIN_H_
-#def _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct convert - struct for print function
+ * @fmt: index
+ * @print: pointer for print function
+ *
+ * Description: stores pointers
+ */
+typedef struct pt_fmt
+{
+	char *fmt;
+	int (*print)(va_list valist);
+} pt_fmt;
+
+
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int get_print(const char *, va_list, pt_fmt pt_format[]);
+
+
 #endif
 
