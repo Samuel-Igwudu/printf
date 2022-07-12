a65095cf9f26ff33c44c2b02dd29965843f54ab1
Deynjiru254 <deynjiru@gmail.com>
2022-07-13 01:09:15 +0300

    a function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -14,6 +14,11 @@ int _printf(const char *format, ...)
 		{"d", print_digit},
 		{"i", print_digit},
 		{"%", print_percent},
+		{"b", func_binary_convert},
+		{"u", func_unsig_int},
+		{"o", func_octal_convert},
+		{"x", func_hex_Lowcase_convert},
+		{"X", func_hex_Upcase_convert},
 		{NULL, NULL}
 	};
 
